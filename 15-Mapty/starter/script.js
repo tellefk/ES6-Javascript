a9ba6e8ad4a1e7cf75309b7993aea7197afd838b
tellefk <tellef94@hotmail.com>
2022-06-17 14:15:26 +0200

    start on map project

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,54 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        const {latitude:lat, longitude:lon} = position.coords
+        // console.log(position.coords.latitude, position.coords.longitude)
+        const map = L.map('map').setView([lat, lon], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    
+        L.marker([lat, lon]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+        console.log(lat, lon)
+        map.on("click",function(mapEvent){
+            console.log(mapEvent)
+            const {lat:Lat, lng:Lon}=mapEvent.latlng
+            console.log(Lat,Lon)
+
+        L.marker([Lat, Lon]).addTo(map).bindPopup(L.popup({
+            maxWidth:250, 
+            maxHeight:150,
+            autoClose:false,
+            closeOnClick:false,
+            className:"running-popup"})).setPopupContent("OK").openPopup()
+    
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }, function() {alert("Cant find position, set to standard location")}
+    )
+}
+
