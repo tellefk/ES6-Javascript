cacd7e0e74f3c336250d9f4ba238ff45b7552ff1
tellefk <tellef94@hotmail.com>
2022-07-12 13:50:13 +0200

    async

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -221,7 +221,7 @@ wait(2).then(() =>{
 // 6. After the 2 seconds have passed, hide the current image.
 
 
-const img=document.createElement('img')
+// const img=document.createElement('img')
 const imageContainer=document.querySelector(".images")
 console.log(imageContainer)
 
@@ -238,10 +238,11 @@ const createImage = function(imgPath){
 }
 
 
-const createImagE = function(imgPath){
+const createImagE = async function(imgPath){
     return new Promise(function(resolve, reject){
+        const img=document.createElement('img') 
         img.style.display="flex"
-        img.src=imgPath
+        img.src=imgPath 
         img.addEventListener("load",function(){
         imageContainer.appendChild(img)
         resolve(img)
@@ -252,21 +253,94 @@ const createImagE = function(imgPath){
     })
 }
 
-createImagE(`https://a-mobler.no/~/pir/products/ptl912026/PTL912026-TL912026.jpg`)
-.then(()=>wait(2))
-.then(()=>{
+// createImagE(`https://a-mobler.no/~/pir/products/ptl912026/PTL912026-TL912026.jpg`)
+// .then(()=>wait(2))
+// .then(()=>{
+//     img.style.display="none"
+//     return createImagE(`https://www.guru-utvikling.no/wp-content/uploads/2017/05/Bilde-til-sak-om-bilder.jpg`)
+// })
+// .then(()=>{
+//     return wait(2)
+// })
+// .then(()=>img.style.display="none")
+
+// Coding Challenge #3
+
+/* 
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+*/
+
+// const loadNPause= async function(img){
+//     let img=await createImagE(`https://a-mobler.no/~/pir/products/ptl912026/PTL912026-TL912026.jpg`)
+//     await wait(2)
+//     img.style.display="none"
+//     img =await createImagE(`https://www.guru-utvikling.no/wp-content/uploads/2017/05/Bilde-til-sak-om-bilder.jpg`)
+//     await wait(2)
+//     img.style.display="none"
+// }
+
+
+const loadImage = async function(imgPath){
+    return new Promise(function(resolve, reject){
+        const img=document.createElement('img')
+        img.style.display="flex"
+        img.src=imgPath
+        img.addEventListener("load",function(){
+        // console.log(`Done loading img type ${img}}`)
+        resolve(img)
+        })
+        img.addEventListener("error",function (){
+            reject(new Error("loding failed "))
+        })
+    })
+}
+
+
+
+
+const loadNPause= async function(imgUrl,sec){
+    await createImagE(imgUrl)
+    await wait(sec)
     img.style.display="none"
-    return createImagE(`https://www.guru-utvikling.no/wp-content/uploads/2017/05/Bilde-til-sak-om-bilder.jpg`)
-})
-.then(()=>{
-    return wait(2)
-})
-.then(()=>img.style.display="none")
+    return img
+}
 
 
 
+const imgArr=["https://a-mobler.no/~/pir/products/ptl912026/PTL912026-TL912026.jpg","https://www.guru-utvikling.no/wp-content/uploads/2017/05/Bilde-til-sak-om-bilder.jpg"]
+
+//function for async loading pictures! 
+const loadAll=async function(imgArray){
+    try {
+        const newImg=imgArray.map(async (img) =>await loadImage(img))
+        console.log(newImg)
+        // henter bildene, loader de og legger dem i  en array. Kan da benytte promise all på alle i paraell med promise.All
+        const promises=await Promise.all(newImg)
+        promises.forEach(async(promise) =>{
+            imageContainer.appendChild(promise)})
+        }catch (err) {
+            console.log(err)
+        }
+}
 
 
 
+// loadNPause()
 
 
+// console.log("run first")
+// loadNPause(imgArr[0],4)
+// console.log("run seceond")
+// loadNPause(imgArr[1],3)
+
+
+loadAll(imgArr)
